16972cb8c7d9874209afb66de54c19db97023ff1
OrbitalOyster <orbitaloyster@gmail.com>
2023-08-23 14:46:42 +0300

    Little clean up

--- dtest/units/02-dmath.c
+++ dtest/units/02-dmath.c
@@ -5,8 +5,18 @@
 
 int main() {
   DTEST_UNIT_START("dmath");
+  double inf = 1 / .0;
+  double nan = .0 / .0;
   DTEST_EVAL_DOUBLE(.1 + .2);
   DTEST_EXPECT_TRUE(compare(.1 + .2, .3));
+  DTEST_EXPECT_TRUE(compare(DMATH_EPSILON, DMATH_EPSILON / 2));
+  DTEST_EXPECT_FALSE(compare(DMATH_EPSILON, DMATH_EPSILON * 2));
+  DTEST_EXPECT_FALSE(compare(.1, nan));
+  DTEST_EXPECT_FALSE(compare(inf, nan));
+  DTEST_EXPECT_FALSE(compare(nan, nan));
+  DTEST_EXPECT_TRUE(compare(inf, inf));
+  DTEST_EXPECT_TRUE(compare(-inf, -inf));
+  DTEST_EXPECT_FALSE(compare(inf, -inf));
   DTEST_UNIT_END
   return 0;
 }
