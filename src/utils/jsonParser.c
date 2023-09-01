3914304862f8f9ca4cf2547d020e64252636c245
OrbitalOyster <orbitaloyster@gmail.com>
2023-09-01 15:58:45 +0300

    Progress on JSON parser

--- src/utils/jsonParser.c
+++ src/utils/jsonParser.c
@@ -4,19 +4,35 @@
 
 #include "utils/debug.h"
 
-enum Token { Undefined, Object, Array, Number, String, Boolean, Eof };
+enum TokenType { Undefined, Object, Array, Number, String, Boolean, Eof };
+
+union TokenValue {
+  int Number;
+  char *String;
+  int Boolean;
+};
+
+struct Token {
+  enum TokenType type;
+  union TokenValue value;
+};
+
+struct TokenMap {
+  int length;
+  struct Token *content;
+};
 
 #define MAX_KEY_LENGTH 255
 #define MAX_STRING_LENGTH 255
 
-void processToken(enum Token type, FILE *f);
+void processToken(enum TokenType type, FILE *f);
 
 int isWhiteSpace(int c) { return (c == ' ' || c == '\n' || c == '\t'); }
 
 int isDigit(int c) { return (c >= '0' && c <= '9'); }
 
-enum Token identifyToken(int c) {
-  enum Token result = Undefined;
+enum TokenType identifyToken(int c) {
+  enum TokenType result = Undefined;
   switch (c) {
   case EOF:
     result = Eof;
@@ -32,11 +48,17 @@ enum Token identifyToken(int c) {
     result = String;
     INFO("Token: String");
     break;
+  case 't':
+  case 'f':
+    result = Boolean;
+    INFO("Token: Boolean");
+    break;
   default:
     if (isDigit(c)) {
       if (c == '0')
         ERR(1, "Invalid number format");
       result = Number;
+      INFO("Token: Number");
     }
     break;
   }
@@ -91,6 +113,36 @@ void readNumber(FILE *f) {
   fseek(f, -1, SEEK_CUR);
 }
 
+void readBoolean(FILE *f) {
+  char *boolean = calloc(5, sizeof(int));
+  int c, n = 0;
+  int done = 0;
+  int readingTrue = 0;
+  const char *trueString = "true";
+  const char *falseString = "false";
+  fseek(f, -1, SEEK_CUR);
+  do {
+    c = fgetc(f);
+    INFOF("Read char: %i [%c]", c, c);
+    if (!n && c == 't')
+      readingTrue = 1;
+    boolean[n] = (char)c;
+    if (readingTrue) {
+      if (c != trueString[n])
+        ERRF(1, "Expected 'true', got '%c'", c);
+      if (n == 3)
+        done = 1;
+    } else {
+      if (c != falseString[n])
+        ERRF(1, "Expected 'false', got '%c'", c);
+      if (n == 4)
+        done = 1;
+    }
+    n++;
+  } while (!done);
+  INFOF("Read Boolean: %s", boolean);
+}
+
 int findNextKey(FILE *f, int expectingComma) {
   int c;
   int done = 0;
@@ -129,20 +181,23 @@ void findNextColon(FILE *f) {
   INFO("Found colon");
 }
 
-enum Token findNextToken(FILE *f, int expectingComma) {
-  enum Token nextToken = Undefined;
+enum TokenType findNextToken(FILE *f, int expectingComma) {
+  enum TokenType nextToken = Undefined;
   int c;
   do {
     c = fgetc(f);
     if (isWhiteSpace(c))
       continue;
     if (c == ',') {
-      if (!expectingComma) ERR( 1, "Unexpected ','");
+      if (!expectingComma)
+        ERR(1, "Unexpected ','");
       expectingComma = 0;
       continue;
     }
-    INFOF("Read char: %i (%c)", c, c);
+    INFOF("Read char: %i [%c]", c, c);
     nextToken = identifyToken(c);
+    if (nextToken == Undefined)
+      ERR(1, "Unexected character");
   } while (nextToken == Undefined);
 
   return nextToken;
@@ -157,16 +212,17 @@ void processObject(FILE *f) {
     else {
       readKey(f);
       findNextColon(f);
-      enum Token nextToken = findNextToken(f, 0);
+      enum TokenType nextToken = findNextToken(f, 0);
       processToken(nextToken, f);
       expectingComma = 1;
     }
   } while (!done);
 }
 
-int findNextArrayElement(FILE* f, int expectingComma) {
+int processNextArrayElement(FILE *f, int expectingComma) {
   int c;
   int done = 0;
+  enum TokenType nextElement = Undefined;
   do {
     c = fgetc(f);
     if (isWhiteSpace(c))
@@ -174,12 +230,16 @@ int findNextArrayElement(FILE* f, int expectingComma) {
     if (c == EOF)
       ERR(1, "Unexpected EOF");
     if (c == ',') {
-      if (!expectingComma) ERR(1, "Unexpected ','");
+      if (!expectingComma)
+        ERR(1, "Unexpected ','");
       expectingComma = 0;
       continue;
     }
-    if (c == ']') return 0;
-    INFOF("Read char: %i (%c)", c, c);
+    if (c == ']')
+      return 0;
+    INFOF("Read char: %i [%c]", c, c);
+    nextElement = identifyToken(c);
+    processToken(nextElement, f);
     done = 1;
   } while (!done);
   return 1;
@@ -188,18 +248,15 @@ int findNextArrayElement(FILE* f, int expectingComma) {
 void processArray(FILE *f) {
   int done = 0;
   int comma = 0;
-  enum Token nextElement = Undefined;
   do {
-    if (!findNextArrayElement(f, comma)) done = 1;
-    else {
+    if (!processNextArrayElement(f, comma))
+      done = 1;
+    else
       comma = 1;
-      nextElement = identifyToken(c);
-    }
-
   } while (!done);
 }
 
-void processToken(enum Token type, FILE *f) {
+void processToken(enum TokenType type, FILE *f) {
   switch (type) {
   case Undefined:
     break;
@@ -220,6 +277,8 @@ void processToken(enum Token type, FILE *f) {
     readString(f);
     break;
   case Boolean:
+    INFO("Processing Boolean...");
+    readBoolean(f);
     break;
   case Eof:
     fclose(f);
@@ -230,6 +289,6 @@ void processToken(enum Token type, FILE *f) {
 void readFile(char *filename) {
   FILE *f;
   f = fopen(filename, "r");
-  enum Token nextToken = findNextToken(f, 0);
+  enum TokenType nextToken = findNextToken(f, 0);
   processToken(nextToken, f);
 }
