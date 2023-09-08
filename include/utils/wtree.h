724eaaf70a41ddc8ddefb77e6215b02f7456289e
OrbitalOyster <orbitaloyster@gmail.com>
2023-09-08 09:10:36 +0300

    wtree

---
+++ include/utils/wtree.h
@@ -0,0 +1,11 @@
+#ifndef WTREE_H
+#define WTREE_H
+
+struct WTreeNode {
+};
+
+struct WTree {
+
+};
+
+#endif
---
+++ src/utils/wtree.c
@@ -0,0 +1,7 @@
+#include "utils/wtree.h"
+
+#include <stdlib.h>
+
+#include "utils/debug.h"
+
+
